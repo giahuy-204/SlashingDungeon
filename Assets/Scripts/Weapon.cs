ddd86528cc3da2fa5ce0b892a44ae7b85b77986d
Huy Dang <giahuy.dng@gmail.com>
2022-12-16 22:54:31 +0700

    add slash anim - part 1

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
+    Animator animator;
+    private bool isAttacking = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = gameObject.GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            isAttacking = true;
+            animator.SetBool("isAttacking", isAttacking);
+        }
+        else
+        {
+            isAttacking = false;
+            animator.SetBool("isAttacking", isAttacking);
+        }
     }
 }
